fac36de3deb14c0943092ee57780eaee81681f98
Anurag Dutta <imanurag18@gmail.com>
2023-10-01 13:04:43 +0530

    Update que.sql

--- que.sql
+++ que.sql
@@ -1,4 +1,27 @@
+Q. List all distinct Bookcases.
 SELECT DISTINCT PRODUCTDESC
 FROM PRODUCT
 WHERE PRODUCTDESC LIKE '%Bookcase';
 
+Q. How many products are there?
+select count(productdesc)
+from product;
+
+Q. List every product finish having unit price > 200 and average of qtyonhand greater than 3.
+select productfinish,avg(qtyonhand) as average
+from product
+where unitprice > 200
+group by productfinish
+having avg(qtyonhand)>3;
+
+Q. List product name(s) having maximum quantity on hand
+select productfinish
+from product
+where qtyonhand=(select max(qtyonhand)from product);
+
+Q. List all products whose unitprice is same as some made of ‘Oak’
+select productfinish
+from product
+where unitprice in (select unitprice from product where productfinish='Oak');
+
+Q. 
